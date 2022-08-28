dba9aed0bda1ca52efdb354bf4e759f4f1dd8fc7
Elie Gazel <elie@gazel.net>
2022-08-28 15:03:54 +0200

    wip bluetooth HC05 connexion

--- windshield_hud.ino
+++ windshield_hud.ino
@@ -1,29 +1,68 @@
 #include "SevSeg.h"
 #include "String.h"
+#include <SoftwareSerial.h> 
 
-SevSeg sevseg; 
+SevSeg sevseg;
 SevSeg sevseg2;
 SevSeg sevseg3;
+SoftwareSerial HC05(11, 12);
+// e0:dc:ff:f2:93:06 -> mac address of my phone
 
 void setup()
 {
+  Serial.begin(9600); 
+  HC05.begin(9600);
+
+  while (!Serial) 
+  {
+    ;
+  }
+  Serial.println("Ready to connect\nDefault password is 1234\n"); 
+  Serial.println("AT+RESET\n");
+  Serial.println("AT+ROLE=1\n");
+  Serial.println("AT+CMODE=0\n");
+  Serial.println("AT+INIT\n");
+  Serial.println("AT+BIND=0010,CC,4F3603\n"); //Replace with MAC Address of ELM327
+  Serial.println("AT+PAIR=0010,CC,4F3603,20\n"); //Replace with MAC Address of ELM327, 20 is the timeout value
+  Serial.println("AT+LINK=0010,CC,4F3603\n"); //Replace with MAC Address of ELM327
+
   byte numDigits = 1;
   byte digitPins[] = {};
+
   // Segment pins are switched to be readable in a reflexion: 7<->6  4<->3  5<->2
   byte segmentPins1[] = {5, 4, 3, 2, 7, 6, 8};
   byte segmentPins2[] = {25, 24, 23, 22, 27, 26, 28};
   byte segmentPins3[] = {32, 33, 30, 31, 35, 34, 36};
 
   bool resistorsOnSegments = true;
-  byte hardwareConfig = COMMON_ANODE; 
-  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins1, resistorsOnSegments);
-  sevseg2.begin(hardwareConfig, numDigits, digitPins, segmentPins2, resistorsOnSegments);
-  sevseg3.begin(hardwareConfig, numDigits, digitPins, segmentPins3, resistorsOnSegments);
+  byte hardwareConfigAnode = COMMON_ANODE; 
+  byte hardwareConfigCathode = COMMON_CATHODE; 
+
+  sevseg.begin(hardwareConfigAnode, numDigits, digitPins, segmentPins1, resistorsOnSegments);
+  sevseg2.begin(hardwareConfigAnode, numDigits, digitPins, segmentPins2, resistorsOnSegments);
+  sevseg3.begin(hardwareConfigAnode, numDigits, digitPins, segmentPins3, resistorsOnSegments);
 }
 
 void loop()
 {
-  displayKmh();
+  if (HC05.available()) {
+    Serial.write(HC05.read());
+  }
+  if (Serial.available()) {
+    HC05.write(Serial.read());
+  }
+  // displayKmh();
+  sevseg.setNumber(4);
+  sevseg2.setNumber(9);
+  sevseg3.setNumber(-1);
+
+  sevseg.setBrightness(1); // -200 to 200
+  sevseg2.setBrightness(1); // -200 to 200
+  sevseg3.setBrightness(1); // -200 to 200
+
+  sevseg.refreshDisplay();
+  sevseg2.refreshDisplay();  
+  sevseg3.refreshDisplay();  
 }
 
 void displayKmh()
@@ -51,9 +90,9 @@ void displayKmh()
       sevseg3.setNumber(buffer[2] - '0');
     }
     
-    sevseg.setBrightness(-200); // -200 to 200
-    sevseg2.setBrightness(-200); // -200 to 200
-    sevseg3.setBrightness(-200); // -200 to 200
+    sevseg.setBrightness(200); // -200 to 200
+    sevseg2.setBrightness(200); // -200 to 200
+    sevseg3.setBrightness(200); // -200 to 200
   
     sevseg.refreshDisplay();
     sevseg2.refreshDisplay();  
